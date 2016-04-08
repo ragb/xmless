dc3e43dc9919e41e4fd9c0bda43c36a6ea87c6f6
Rui Batista <ruiandrebatista@gmail.com>
2016-04-08 15:27:03 +0100

    Add ToXml unit test.

---
+++ src/test/scala/xmless/ToXmlTest.scala
@@ -0,0 +1,25 @@
+package xmless
+
+import org.scalatest._
+import scala.xml._
+import xmless.syntax.toXml._
+import Defaults._
+import cats.syntax.option._
+
+class ToXmlTest extends FlatSpec with Matchers {
+  def serialize(xml: NodeSeq) = xml.toString
+
+  "ToXml" should "Serialize primitive values to XML" in {
+    serialize(1.toXml) shouldBe "1"
+    serialize("hello".toXml) shouldBe "hello"
+  }
+
+  it should "Serialize option types" in {
+    serialize(1.some.toXml) shouldBe "1"
+    serialize((None: Option[Int]).toXml) shouldBe ""
+  }
+
+  it should "Serialize sequences of strings" in {
+    serialize(List(1, 2, 3).toXml) shouldBe "123"
+  }
+}
